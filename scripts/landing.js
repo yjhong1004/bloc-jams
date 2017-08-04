87b24500fccc0a1c816e00471c247b7180509608
Jenny Hong <yjhong1004@gmail.com>
2017-08-04 14:23:21 -0700

    checkpoint 9 changes

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,30 @@
-var animatePoints = function() {
-
-var points = document.getElementsByClassName('point');
-
-var revealPoint = function (index) {
-  points[index].style.opacity = 1;
-  points[index].style.transform = "scaleX(1) translateY(0)";
-  points[index].style.msTransform = "scaleX(1) translateY(0)";
-  points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+var pointsArray = document.getElementsByClassName('point');
+var revealPoint = function (point) {
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(0)";
+  point.style.msTransform = "scaleX(1) translateY(0)";
+  point.style.WebkitTransform = "scaleX(1) translateY(0)";
 }
-
-for (var i =0; i < points.length; i++) {
-  revealPoint(i);
+function forEach (array, callback) {
+  for (var i = 0; i < array.length; i++) {
+    callback(array[i]);
+  }
 }
+var animatePoints = function(points) {
+    forEach(points, revealPoint);
 
 };
-animatePoints();
+
+
+window.onload = function() {
+  if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+   var sellingPoints = document.getElementsByClassName('selling-points')[0];
+var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+          animatePoints(pointsArray);
+      }
+  });
+ }

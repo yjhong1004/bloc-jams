b315186a9060aff2a0f6fb817f8b259ec57958cb
Jenny Hong <yjhong1004@gmail.com>
2017-08-07 04:10:08 -0700

    checkpoint 12 changes

--- scripts/album.js
+++ scripts/album.js
@@ -47,7 +47,7 @@ var albumPicasso = {
  var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
-     + '  <td class="song-item-number">' + songNumber + '</td>'
+     + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="song-item-title">' + songName + '</td>'
      + '  <td class="song-item-duration">' + songLength + '</td>'
      + '</tr>'
@@ -84,10 +84,23 @@ var setCurrentAlbum = function(album) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
-
- window.onload = function() {
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+window.onload = function() {
      setCurrentAlbum(albumPicasso);
 
+     songListContainer.addEventListener('mouseover', function(event) {
+         // #1
+         if (event.target.parentElement.className === 'album-view-song-item') {
+         event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+     for (var i = 0; i < songRows.length; i++) {
+              songRows[i].addEventListener('mouseleave', function(event) {
+              this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+              });
+          }
 
 var albumList = [albumPicasso, albumMarconi, albumJenny];
 var index = 0;

a13d38b3b1961038e9f0b0f24db1c3a95c81abe3
Jenny Hong <yjhong1004@gmail.com>
2017-08-21 04:38:47 -0700

    assignment 19 changes

--- scripts/album.js
+++ scripts/album.js
@@ -1,3 +1,11 @@
+var setSong = function (songNumber) {
+  currentlyPlayingSongNumber = parseInt(songNumber);
+  currentSongFromAlbum = currentAlbum.songs[songNumber-1];
+};
+var getSongNumberCell = function(number){
+  return $('.song-item-number[data-song-number="' + number + '"]');
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
@@ -13,21 +21,19 @@ var clickHandler = function() {
   var songNumber = parseInt($(this).attr('data-song-number'));
     if (currentlyPlayingSongNumber !== null) {
   // Revert to song number for currently playing song because user started playing new song.
-      var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+      var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
        currentlyPlayingCell.html(currentlyPlayingSongNumber);
 }
     if (currentlyPlayingSongNumber !== songNumber) {
   // Switch from Play -> Pause button to indicate new song is playing.
      $(this).html(pauseButtonTemplate);
-      currentlyPlayingSongNumber = songNumber;
-      currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+      setSong(songNumber);
       updatePlayerBarSong();
 }   else if (currentlyPlayingSongNumber === songNumber) {
   // Switch from Pause -> Play button to pause currently playing song.
     $(this).html(playButtonTemplate);
     $('.main-controls .play-pause').html(playerBarPlayButton);
-     currentlyPlayingSongNumber = null;
-     currentSongFromAlbum = null;
+     setSong(songNumber);
     }
 };
 
